de9e57bb3f4448cb451973c02ecd512bb0767ba4
Harrison Jones <montgomerymediaco@gmail.com>
2025-10-06 12:50:21 +0400

    v.17.12 (pay walls in correct locations)

--- services/paywallService.ts
+++ services/paywallService.ts
@@ -83,8 +83,7 @@ class PaywallService {
           placement = 'campaign_trigger';
           break;
         case 'stripe_button':
-          // Stripe button should mirror the main upgrade placement that is known to show a paywall
-          placement = 'create_item_limit';
+          placement = 'stripe_button';
           break;
         case 'ai_pay_wall':
           placement = 'ai_pay_wall';
